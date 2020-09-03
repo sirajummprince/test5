36d369ee51f250ebcc661f18631b787106edf479
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2020-09-03 23:05:37 +0600

    print dictionary with foreach

---
+++ question_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace question_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> d = new Dictionary<string, string>()
+            {
+                {"James Bond","Shaken, not stirred"},
+                {"Miss Moneypenny", "Computer Science"},
+                {"DR. No","Being Evil"},
+            };
+
+            foreach (KeyValuePair<string, string> dict in d)
+            {
+                Console.WriteLine("{0}:{1}", dict.Key, dict.Value);
+            }
+        }
+    }
+}
