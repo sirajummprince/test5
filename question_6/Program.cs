cc36cdcd8061b35150c90907a8c0187bec8ac904
Sirajum Munir Prince <sirajummunirprince@gmail.com>
2020-09-03 23:04:30 +0600

    fibonacci numbber

---
+++ question_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace question_6
+{
+    class Program
+    {
+        public static void fibonacci(int n)
+        {
+            int zero = 0, first = 1, sum = 0;
+
+            Console.WriteLine("{0} \n{1}", zero, first);
+
+            for (int i = 2; i < n; i++)
+            {
+                sum = zero + first;
+                Console.WriteLine("{0}", sum);
+                zero = first;
+                first = sum;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            fibonacci(N);
+        }
+    }
+}
